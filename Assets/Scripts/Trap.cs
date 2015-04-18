2161e8d57f42320c6a6311dda08a7c99fd7fc8d1
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2015-04-18 20:16:14 +0200

    Traps

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Trap : MonoBehaviour {
+
+	private Color clrBase;
+	void Awake () {
+	    clrBase = GetComponent<Renderer>().material.color;
+        objectsInside = new List<GameObject>();
+	}
+
+
+	void OnMouseDown() {
+        foreach (GameObject o in objectsInside) {
+            Destroy(o);
+        }
+	}
+    
+    private List<GameObject> objectsInside;
+    void OnTriggerEnter(Collider other) {
+        objectsInside.Add(other.gameObject);
+    }
+    void OnTriggerExit(Collider other) {
+        objectsInside.Remove(other.gameObject);
+    }
+
+	void OnMouseEnter() {
+        GetComponent<Renderer>().material.color = new Color(1.0F, 0.87F, 0.75F);
+	}
+
+	void OnMouseExit() {
+        GetComponent<Renderer>().material.color = clrBase;
+	}
+}

768bdb223de578e7746c2fef1a27bbd88b5b052e
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2015-04-18 17:38:29 +0200

    Continuous creep spawning

---
+++ Assets/Scripts/Creep.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creep : MonoBehaviour {
+
+    public GameObject target;
+    private NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Awake () {
+	    navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (target != null) {
+            navAgent.destination = target.transform.position;
+        }
+	}
+}
---
+++ Assets/Scripts/CreepSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreepSpawner : MonoBehaviour {
+
+    private GameObject target;
+    static private Object creepRes;
+	void Awake () {
+        target = GameObject.Find("boss");
+        if(creepRes == null) creepRes = Resources.Load("creep");
+	}
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("TrySpawn", 0.0f, 1.0f);
+	}
+
+    private void TrySpawn() {
+        if (Random.Range(0, 4) == 0){
+            SpawnOnce();
+        }
+    }
+
+    public void SpawnOnce () {
+        GameObject creep = (GameObject)Instantiate(creepRes);
+        creep.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        creep.GetComponentInChildren<Creep>().target = target;
+    }
+}
